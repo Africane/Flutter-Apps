3d6ea88088e47183c3737e89813878f3dd8816a3
David Owino <daveowino753@gmail.com>
2024-10-14 10:28:15 +0300

    added new project, notes app

---
+++ mynotesapp/android/app/src/main/kotlin/com/mynotesapp/mynotesapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mynotesapp.mynotesapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mynotesapp/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required String title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Column(
+        children: [
+          TextField(),
+          TextField(),
+          TextButton(
+            onPressed: () async { },
+          child: const Text('Register'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
