0d184599aa60c2e4c5904d5a4f8d95de793bd0ca
David Owino <daveowino753@gmail.com>
2024-11-27 11:17:35 +0300

    added appwrite constants and assets

---
+++ twitter_clone/lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '6746cba70007db922c7e';
+  static const String projectId = '67407911001356d6bf05';
+  static const String endPoint = 'http://localhost:80';
+}
