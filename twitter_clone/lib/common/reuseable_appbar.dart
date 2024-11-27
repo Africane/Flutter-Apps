7edf1692011fba38340fd2aef112a1e6886a9bd8
David Owino <daveowino753@gmail.com>
2024-11-27 12:47:25 +0300

    added files

---
+++ twitter_clone/lib/common/reuseable_appbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReUseableAppBar extends StatelessWidget {
+  const ReUseableAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar();
+  }
+}
