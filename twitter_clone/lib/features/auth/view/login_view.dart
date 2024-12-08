831f5fbf90b089ccb4f0f31c2a758d48ab134bcb
David Owino <daveowino753@gmail.com>
2024-12-08 15:33:32 +0300

    amended update

--- twitter_clone/lib/features/auth/view/login_view.dart
+++ twitter_clone/lib/features/auth/view/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/constants/ui_constants.dart';
+import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -10,16 +11,28 @@ class LoginView extends StatefulWidget {
 
 class _LoginViewState extends State<LoginView> {
   final appbar = UiConstants.appBar();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appbar,
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(children: [
             //textfield1
+            AuthField(controller: emailController),
             //textfield2
+            AuthField(controller: passwordController),
             //button
             //textspan
           ],),
